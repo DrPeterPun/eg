cba8a72a0276664b4ae817d5103b91f410737791
DrPeterPun <pedro_pereira@outlook.pt>
2022-03-21 14:23:51 +0000

    aula de 21 mar

--- tp3/compilador.py
+++ tp3/compilador.py
@@ -13,9 +13,10 @@ struct : array | lista | set
 array: type "[" NUMBER "]"
 set: "SET<" type ">"
 lista: "LIST<" type ">"
-contrl: cif | cwhile
+contrl: (nestedcif | cif | cwhile) ";"
 cif : "if(" conds "){" expression* "}"
-cwhile : "while (" conds "){" expression* "}"
+nestedcif.1 : "if(" conds "){" "if(" conds "){" expression* "}" "}"
+cwhile : "while(" conds "){" expression* "}"
 conds : cond (CONDOP cond)*
 cond : BOOL | arit COMPOP arit | var
 BOOL : "true" | "false"
@@ -48,8 +49,26 @@ class Transf(Transformer):
     def defs(self,item):
         return item
 
+    def expression(self,item):
+        #print(str(item)+"\n")
+        return item
+
+    def contrl(self,item):
+        print("///////////////////////////////////////////////////////////////////////////////////////////")
+        print(str(item)+"\n")
+        print(str(item[0])+"\n")
+        return item
+
+    def nestedcif(self,item):
+        print("lsadkjfa;sldfja;sldfjkasdlkfjas;fja;1n1n1n1n\n\nn\n\n\nasdasdf")
+
     def cif(self,item):
-        self.count+=1
+        #self.count+=1
+        #print("///////////////////////////////////////////////////////////////////////////////////////////")
+        #print(str(item)+"\n")
+        #print(str(item[0])+"\n")
+        #print(str(item[1])+"\n")
+        #print(item[1].children[0])
         return item
 
     def atrib(self,item):
@@ -58,34 +77,41 @@ class Transf(Transformer):
     def deff(self,item):
         self.vars.update({item[1] : False})
         return item
+    pass
 
     def defplus(self,item):
-        self.vars.update({item[1] : True})
+        self.vars.update({item[1][0] : True})
         return item
 
     def var(self,item):
-        print("nome da var:")
-        print(item[0])
-        print("init?")
-        if(self.vars.get(item[0])):
-            print("yes")
+        #print("nome da var:")
+        #print(item[0])
+        #print("init?")
+        if(self.vars.get(item[0][0])):
+            pass
+            #print("yes")
         else:
-            print("no!")
+            #print("no!")
             self.vars.update({item[0] : False})
-        return item[0]
+        return item
 
     def varinit(self,item):
         #print("var:")
         #print(str(item[0]))
-        return item[0]
+        return item
 
-l = Lark(grammar)
+l = Lark(grammar,keep_all_tokens=False)
 inp = open("test")
-f= inp.read()
+f = inp.read()
 tree = l.parse(f)
+tree.pretty()
 t = Transf()
+tree.pretty(indent_str=" ")
 data = t.transform(tree)
+print(tree)
+
 for a in t.vars:
-    if(not t.vars.get(a)):
-        print(str(a) + " nao inicializado")
-print("count " + str(t.count))
+    #if(not t.vars.get(a)):
+        #print(str(a) + " nao inicializado")
+    pass
+#print("count " + str(t.count))
